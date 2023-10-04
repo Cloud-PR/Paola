af6f510daafda3794a750ed8d283a0d2e4538549
Cloud-PR <polaposa.1@gmail.com>
2023-10-04 10:01:50 -0500

    UpdatedVariableWarmup

--- Oct4Warmup/src/main/java/org/example/Main.java
+++ Oct4Warmup/src/main/java/org/example/Main.java
@@ -5,10 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         String personName = "John Doe";
+        System.out.println(personName);
+
         int personAge = 25;
+        System.out.println(personAge);
+
         double productPrice = 19.99;
+        System.out.println(productPrice);
+
         float temperatureCelsius = 27.5f;
+        System.out.println(temperatureCelsius);
+
         boolean isLoggedIn = true;
+        System.out.println(isLoggedIn);
 
     }
 }
